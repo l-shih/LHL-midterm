b2d693550f75a6165462b1c71e1bb33b39bed98d
Lilian Shih <l.shih@outlook.com>
2017-11-22 21:18:11 +0000

    adding tables items and restaurants

---
+++ db/migrations/20171122211453_create_items_table_with_restaurant_fk.js
@@ -0,0 +1,18 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('items', function (table) {
+      table.increments();
+      table.string('type');
+      table.string('title').notNullable();
+      table.string('description').notNullable();
+      table.float('price', 8, 2).notNullable();
+  
+      table.integer('restaurant_id');
+      table.foreign('restaurant_id').reference('restaurant.id');
+      });
+  };
+  
+  exports.down = function(knex, Promise) {
+    return knex.schema.dropTable('items');
+  };
+  
