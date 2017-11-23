c30b48e810e464e4561da4b73a1c2337b5a2ec6c
Kang <kwang.inbox@gmail.com>
2017-11-23 00:12:30 +0000

    JS for orders review details.

---
+++ public/scripts/orders_review_page.js
@@ -0,0 +1,13 @@
+$(() => {
+  function createOrdersReviewElement(order) {
+    var order = `<article class='order'>
+                  <header class='header'>
+                    <p>This is the header of the Order Review</p>
+                  </header>
+                  <main='order_details'>
+                    <p>This part is for the order details</p>
+                  </main>
+                </article>`
+    return order;
+  }
+});
