8addb052f09c06b76a0d38e5c750a75f07b18693
chance-gao <chance-gao@hotmail.com>
2017-11-26 21:53:45 -0800

    eslent server.js

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.get("/", (req, res) => {
 
 app.get('/orders', (req, res) => {
   res.sendStatus(404);
-})
+});
 
 // Orders review page:
 app.get('/orders/:id', (req, res) => {
@@ -58,33 +58,33 @@ app.get('/orders/:id', (req, res) => {
         return res.status(404).send('You have not specified an order number! Have you made an order? You should make an order!');
       } else {
         return knex.select('title', 'description', 'price', 'line_items.quantity', 'orders.total_price', 'orders.id', 'orders.est_ready_time')
-                .from('items')
-                .innerJoin('line_items', 'line_items.item_id', 'items.id')
-                .innerJoin('orders', 'line_items.order_id', 'orders.id')
-                .where('orders.id', req.params.id)
-                .asCallback(function(err, result) {
-                  if (err) {
-                    return err;
-                  } else {
-                    return result;
-                  }
-                }).then(function(result) {
+          .from('items')
+          .innerJoin('line_items', 'line_items.item_id', 'items.id')
+          .innerJoin('orders', 'line_items.order_id', 'orders.id')
+          .where('orders.id', req.params.id)
+          .asCallback(function(err, result) {
+            if (err) {
+              return err;
+            } else {
+              return result;
+            }
+          }).then(function(result) {
           // console.log(result);
-          return res.render('orders_review_page', {'result': result});
-        })
+            return res.render('orders_review_page', {'result': result});
+          });
       }
     });
 });
 
 app.get('/o/:id', function redirectToOrders(req, res) {
   knex('orders').select('id').where('id', req.params.id)
-  .then(function(result) {
-    if(result.length){
-      res.redirect('/orders/'+req.params.id);
-    } else{
-      res.send(404);
-    }
-  });
+    .then(function(result) {
+      if(result.length){
+        res.redirect('/orders/' + req.params.id);
+      } else{
+        res.send(404);
+      }
+    });
 });
 
 app.get('/owner', function(req, res) {
@@ -125,85 +125,85 @@ app.post('/menu', (req, res)=>{
     resolve({lastName, firstName, phoneNum, itemQuantity: itemQuantity, totalPrice, SMS: preSMS});
 
   })
-  .then(function(userObj) {
+    .then(function(userObj) {
     //update users
-    knex('users').select(1).where('phone', userObj.phoneNum)
-    .then((rows)=>{
-      if(rows.length){
-        return knex('users')
-          .where('phone', userObj.phoneNum)
-          .update({
-            'first_name': userObj.firstName,
-            'last_name': userObj.lastName
-          })
-          .then(()=>{
-            return knex('users').select('id').where('phone', userObj.phoneNum)
-            .then(function(rows) {
-              let userId = rows[0].id;
-              userObj.userId = userId;
-              return userObj;
-            });
-          });
-      } else{
-        return knex('users')
-          .insert({
-            'phone': userObj.phoneNum,
-            'first_name': userObj.firstName,
-            'last_name': userObj.lastName
-          })
-          .then(function() {
-            return knex('users').select('id').where('phone', userObj.phoneNum)
-            .then(function(rows) {
-              let userId = rows[0].id;
-              userObj.userId = userId;
-              return userObj;
-            });
-          });
-      }
+      knex('users').select(1).where('phone', userObj.phoneNum)
+        .then((rows)=>{
+          if(rows.length){
+            return knex('users')
+              .where('phone', userObj.phoneNum)
+              .update({
+                'first_name': userObj.firstName,
+                'last_name': userObj.lastName
+              })
+              .then(()=>{
+                return knex('users').select('id').where('phone', userObj.phoneNum)
+                  .then(function(rows) {
+                    let userId = rows[0].id;
+                    userObj.userId = userId;
+                    return userObj;
+                  });
+              });
+          } else{
+            return knex('users')
+              .insert({
+                'phone': userObj.phoneNum,
+                'first_name': userObj.firstName,
+                'last_name': userObj.lastName
+              })
+              .then(function() {
+                return knex('users').select('id').where('phone', userObj.phoneNum)
+                  .then(function(rows) {
+                    let userId = rows[0].id;
+                    userObj.userId = userId;
+                    return userObj;
+                  });
+              });
+          }
 
-      //insert orders
-    })
-    .then(function(orderObj) {
-        return knex('orders').insert({
-          'order_id': orderObj.userId,
-          'total_price': orderObj.totalPrice
+          //insert orders
         })
-        .then(function() {
-          return knex('orders').select('id').where('order_id', orderObj.userId)
-          .then(function(rows) {
-            let orderId = rows[rows.length - 1].id;
-            orderObj.orderId = orderId;
+        .then(function(orderObj) {
+          return knex('orders').insert({
+            'order_id': orderObj.userId,
+            'total_price': orderObj.totalPrice
+          })
+            .then(function() {
+              return knex('orders').select('id').where('order_id', orderObj.userId)
+                .then(function(rows) {
+                  let orderId = rows[rows.length - 1].id;
+                  orderObj.orderId = orderId;
 
-            //get order id and send sms
-            client.messages.create({
-              body: orderObj.SMS + ' Order Id: ' + orderId,
-              to: process.env.SMS_to,
-              from: process.env.SMS_from
-            })
+                  //get order id and send sms
+                  client.messages.create({
+                    body: orderObj.SMS + ' Order Id: ' + orderId,
+                    to: process.env.SMS_to,
+                    from: process.env.SMS_from
+                  });
 
-            return orderObj;
-          });
-        });
-
-    })
-    .then(function(itemObj) {
-        for(let key in itemObj.itemQuantity){
-          knex('items').select('id').where(('title'), 'like', `%${key}%`)
-          .then(function(rows) {
-            return knex('line_items').insert({
-              'item_id': rows[0].id,
-              'order_id': itemObj.orderId,
-              'quantity': Number(itemObj.itemQuantity[key])
+                  return orderObj;
+                });
             });
-          });
-        }
-        return itemObj.orderId;
-    })
-    //redirect url
-    .then(function(id) {
-      res.redirect(`/o/${id}`);
+
+        })
+        .then(function(itemObj) {
+          for(let key in itemObj.itemQuantity){
+            knex('items').select('id').where(('title'), 'like', `%${key}%`)
+              .then(function(rows) {
+                return knex('line_items').insert({
+                  'item_id': rows[0].id,
+                  'order_id': itemObj.orderId,
+                  'quantity': Number(itemObj.itemQuantity[key])
+                });
+              });
+          }
+          return itemObj.orderId;
+        })
+      //redirect url
+        .then(function(id) {
+          res.redirect(`/o/${id}`);
+        });
     });
-  });
 });
 
 // owner editing page
@@ -213,38 +213,38 @@ app.post('/owner/accept', (req, res)=>{
     let orderIdTime = req.body.orderIdTime || null;
     let currentTime = new Date().getTime();
     let placeAt = new Date(currentTime).toUTCString();
-    let readyAt = new Date(currentTime + Number(orderIdTime)*60*1000).toUTCString();
+    let readyAt = new Date(currentTime + Number(orderIdTime) * 60 * 1000).toUTCString();
 
     resolve({orderId, placeAt, readyAt, orderIdTime});
   })
-  .then(function(acceptObj) {
-    return knex('orders').where('id', acceptObj.orderId)
-    .update({
-      'paid_at' : acceptObj.placeAt,
-      'ready_at' : acceptObj.readyAt,
-      'est_ready_time' : acceptObj.orderIdTime
+    .then(function(acceptObj) {
+      return knex('orders').where('id', acceptObj.orderId)
+        .update({
+          'paid_at': acceptObj.placeAt,
+          'ready_at': acceptObj.readyAt,
+          'est_ready_time': acceptObj.orderIdTime
+        })
+        .then(function() {
+          return knex('users').distinct('users.phone').select('users.phone')
+            .innerJoin('orders', 'users.id', 'order_id')
+            .where('orders.id', acceptObj.orderId)
+            .then(function(rows) {
+              let userPhone = rows[0].phone;
+
+              let SMS = `Your order has been accepted. It will be ready in ${acceptObj.readyAt}. You can check the order status on /orders/${acceptObj.orderId}`;
+
+              //send SMS to user
+              client.messages.create({
+                body: SMS,
+                to: '+1' + userPhone,
+                from: process.env.SMS_from
+              });
+            });
+        });
     })
     .then(function() {
-       return knex('users').distinct('users.phone').select('users.phone')
-       .innerJoin('orders', 'users.id', 'order_id')
-       .where('orders.id', acceptObj.orderId)
-       .then(function(rows) {
-          let userPhone = rows[0].phone;
-
-          let SMS = `Your order has been accepted. It will be ready in ${acceptObj.readyAt}. You can check the order status on /orders/${acceptObj.orderId}`;
-
-          //send SMS to user
-          client.messages.create({
-            body: SMS,
-            to: '+1'+userPhone,
-            from: process.env.SMS_from
-          })
-       });
+      res.redirect('/owner');
     });
-  })
-  .then(function() {
-    res.redirect('/owner');
-  });
 });
 
 app.post('/owner/add', (req, res)=>{
